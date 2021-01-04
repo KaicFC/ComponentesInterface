82c9a94aa817966218cd294605558f3835fad990
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-04 11:24:58 -0300

    RadioButton e RadioGroup.

--- app/src/main/java/com/cursoandroid/componentesinterface/MainActivity.java
+++ app/src/main/java/com/cursoandroid/componentesinterface/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -15,8 +17,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText campoNome;
     private TextInputEditText campoEmail;
     private TextView textoResultado;
-
     private CheckBox checkVerde, checkBranco, checkVermelho;
+    private RadioButton sexoMasculino, sexoFeminino;
+    private RadioGroup opcaoSexo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,51 @@ public class MainActivity extends AppCompatActivity {
         checkBranco = findViewById(R.id.checkBranco);
         checkVermelho = findViewById(R.id.checkVermelho);
 
+        //RadioButton
+        sexoMasculino = findViewById(R.id.radioButtonMasculino);
+        sexoFeminino = findViewById(R.id.radioButtonFeminino);
+
+        opcaoSexo = findViewById(R.id.radioGroupSexo);
+
+        radioButton();
+    }
+
+    public void radioButton(){
+
+        opcaoSexo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.radioButtonMasculino){
+                    textoResultado.setText("Masculino");
+                }else if(checkedId == R.id.radioButtonFeminino){
+                    textoResultado.setText("Feminino");
+                }
+
+            }
+        });
+
+     /*   if(sexoMasculino.isChecked()){
+            textoResultado.setText("Masculino");
+        }else{
+            textoResultado.setText("Feminino");
+        }
+      */
+
+    }
+
+    public void enviar(View view){
+
+        //checkbox();
+
+        radioButton();
+
+        /*
+        String nome = campoNome.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        textoResultado.setText("Nome: " + nome + ", E-mail: " + email);
+
+     */
     }
 
     public void checkbox(){
@@ -49,19 +97,6 @@ public class MainActivity extends AppCompatActivity {
         }
         textoResultado.setText( texto);
 
-    }
-
-    public void enviar(View view){
-
-        checkbox();
-
-        /*
-        String nome = campoNome.getText().toString();
-        String email = campoEmail.getText().toString();
-
-        textoResultado.setText("Nome: " + nome + ", E-mail: " + email);
-
-     */
     }
 
     public void limpar(View view){
